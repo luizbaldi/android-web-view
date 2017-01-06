3ebcbc70447c020dbf4d3accf4cec38243129644
Luiz Baldi <baldilp@gmail.com>
2017-01-05 23:46:12 -0200

    first version of webview done

--- app/src/main/java/com/luizbaldi/androidwebview/WebView.java
+++ app/src/main/java/com/luizbaldi/androidwebview/WebView.java
@@ -2,12 +2,23 @@ package com.luizbaldi.androidwebview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 
 public class WebView extends AppCompatActivity {
 
+    private android.webkit.WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        mWebView = (android.webkit.WebView) findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Loads the url
+        mWebView.loadUrl("http://google.com/");
     }
 }
